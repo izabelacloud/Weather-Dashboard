3188958019acd38e98f261064b627926afd024ec
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-04-17 15:27:45 -0700

    changes on elements for the 1 day forecast plus icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,19 @@ var cityDisplayEl = document.querySelector("#result-container")
 var cityContainerEl = document.querySelector("#city-container");
 var citySearchTerm = document.querySelector("#city-search-term");
 var iconEl = document.querySelector("#icon");
+var temperatureEl = document.querySelector("#temperature");
+var humidityEl = document.querySelector("#humidity");
+var windspeedEl = document.querySelector("#windspeed");
+var uvindexEl = document.querySelector("#uvindex");
+var uvIndexIconEl = document.querySelector("#uvicon");
 
-var lon = ""
-console.log(lon);
 
-var lat = ""
-console.log(lat);
+
+
+var lat
+var lon
+var uvUrl
+
 
 
 
@@ -22,10 +29,13 @@ var formSubmitHandler = function(event) {
     event.preventDefault();
     var cityName = cityInputEl.value.trim();
     console.log(cityName);
+    var apiFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=813b5c16fd3ec3678dd044ecba8f628a";
 
     if (cityName) {
         //to get the cities with the city name
         getWeatherPerCity(cityName);
+        getFiveDayForecast(apiFiveDayUrl);
+
    
         //to clear the input form field after submit
         cityInputEl.value = "";
@@ -38,13 +48,12 @@ var formSubmitHandler = function(event) {
 
 
 //this function gets the response from the API
-var getWeatherPerCity = function(city, lon, lat) {
+var getWeatherPerCity = function(city) {
     //format the api url
     // var apiUrl = "https://api.github.com/users/" + user + "/repos";
     // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Bratislava&appid=813b5c16fd3ec3678dd044ecba8f628a"
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=813b5c16fd3ec3678dd044ecba8f628a";
-    var apiFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=813b5c16fd3ec3678dd044ecba8f628a";
-    var uvUrl = "https://http://api.openweathermap.org/data/2.5/uvi?appid=813b5c16fd3ec3678dd044ecba8f628a&lat=" + lat + "&lon=" + lon;
+
 
 
 
@@ -101,13 +110,7 @@ var getWeatherPerCity = function(city, lon, lat) {
 
 
 
-            // var dtnew = new Date((dt * 1000) + (36000));
-            // console.log(dtnew);
 
-            // var loc = '-33.87, 151.21' // Sydney expressed as lat,lng tuple
-            // var targetDate = new Date() // Current date/time of user computer
-            // var timestamp = targetDate.getTime()/1000 + targetDate.getTimezoneOffset() * 60 // Current UTC date/time expressed as seconds since midnight, January 1, 1970 UTC
-            //     console.log(timestamp);
 
             var utcSeconds = 1587084500;
             var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
@@ -120,7 +123,7 @@ var getWeatherPerCity = function(city, lon, lat) {
 
 
 
-            //source for codes: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+            //this conditionality handles conditional show and hide of images based on the response --> source for codes: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
             var weatherIcon = jsonResponse.weather[0].main;
             console.log(weatherIcon);
 
@@ -150,43 +153,82 @@ var getWeatherPerCity = function(city, lon, lat) {
             }
 
 
-            var lon = jsonResponse.coord.lon;
-            console.log(lon);
 
-            var lat = jsonResponse.coord.lat;
-            console.log(lat);
 
+            //get temperature data
             var temperature = jsonResponse.main.temp; 
-            console.log(temperature);
-            // var celsius = "℃"
+            console.log(temperature + " ℃");
+
+            //get humidity data
+            var humidity = jsonResponse.main.humidity;
+            console.log(humidity + "%");
+
+            //get wind speed data
+            var windSpeed = jsonResponse.wind.speed;
+            console.log(windSpeed + " MPH");
+
+            //get latitude data
+            lat = jsonResponse.coord.lat;
+            console.log(lat);
+            //get longitude data
+            lon = jsonResponse.coord.lon;
+            console.log(lon);
+            //define URL for UV index based on the retrieved data from the one day weather api and pass it into the URL
+            uvUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=813b5c16fd3ec3678dd044ecba8f628a&lat=" + lat + "&lon=" + lon;
+            //call the getUvIndex function
+            getUvIndex(uvUrl);
+
 
 
             //construct the title line for the current location
             cityContainerEl.innerHTML = cityName + currentDate;
+            //assign the temperatureEl the value from the api call result
+            temperatureEl.innerHTML = "Temperature: " + temperature + " ℃";
+            //assign the humidityEl the value from the api call result            
+            humidityEl.innerHTML = "Humidity: " + humidity + " %";
+            //assign the windspeedEl the value from the api call result    
+            windspeedEl.innerHTML = "Wind Speed: "  + windSpeed + " MPH";
+
+
+
+
+
+
+
             //add the current location to the page
             cityDisplayEl.appendChild(cityContainerEl);
+            //add icon to the page
             cityDisplayEl.appendChild(iconEl);
+            //add temperature to the page
+            cityDisplayEl.appendChild(temperatureEl);
+            //add humidity to the page
+            cityDisplayEl.appendChild(humidityEl);
+            //add windspeed to the page
+            cityDisplayEl.appendChild(windspeedEl);
+
 
 
 
 
             })
             //this is the API call for the UV API
-            .then(function(lat, lon) {
-                fetch(uvUrl).then(function(response){
-                    response.json().then(function (responseUvUrl) {
-                        console.log(responseUvUrl);
-                    })
-                })
-            })
+            // .then(function(response) {
+
+            //     fetch(uvUrl).then(function(response){
+            //         response.json().then(function (responseUvUrl) {
+            //             console.log(responseUvUrl);
+            //         })
+            //     })
+            // })
             //this is the API call for the 5 day forecast
-            .then(function(response) {
-                fetch(apiFiveDayUrl).then(function(response){
-                    response.json().then(function (responseapiFiveDayUrl) {
-                        console.log(responseapiFiveDayUrl);
-                    })
-                })
-            })
+            // .then(function(response) {
+
+            //     fetch(apiFiveDayUrl).then(function(response){
+            //         response.json().then(function (responseapiFiveDayUrl) {
+            //             console.log(responseapiFiveDayUrl);
+            //         })
+            //     })
+            // })
 
             
 
@@ -201,6 +243,78 @@ var getWeatherPerCity = function(city, lon, lat) {
     })
 
 }
+
+
+
+    //this is the API call for the 5 day forecast
+    var getFiveDayForecast = function (apiFiveDayUrl) {
+        fetch(apiFiveDayUrl).then(function(response) {
+            if(response.ok) {
+                response.json().then(function (responseapiFiveDayUrl) {
+                    console.log(responseapiFiveDayUrl);
+                })
+            }
+            else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect to Open Weather Map")
+        })
+    }
+
+
+    //this is the API call for the UV API
+    var getUvIndex = function (uvUrl) {
+        fetch(uvUrl).then(function(response) {
+            if(response.ok) {
+                response.json().then(function (responseUvUrl) {
+                    console.log(responseUvUrl);
+                    //get UV index data
+                    var uvIndex = responseUvUrl.value;
+                    // uvIndex.setAttribute("class", "icon-danger")
+
+                    // uvindexEl.className = "icon-danger";
+                    console.log(uvIndex);
+
+                    //conditionality to add a indicator / image for the UV index color
+                    if (uvIndex >=0 && uvIndex <3) {
+                        uvIndexIconEl.setAttribute("src", "./assets/images/green.png")
+                    } 
+                    else if (uvIndex >=3 && uvIndex < 6) {
+                        uvIndexIconEl.setAttribute("src", "./assets/images/yellow.png")
+                    }
+                    else if (uvIndex >=6 && uvIndex < 8) {
+                        uvIndexIconEl.setAttribute("src", "./assets/images/orange.png")
+                    }
+                    else if (uvIndex >=8 && uvIndex < 11) {
+                        uvIndexIconEl.setAttribute("src", "./assets/images/red.png")
+                    }
+                    else if (uvIndex >=11) {
+                        uvIndexIconEl.setAttribute("src", "./assets/images/violet.png")
+                    }
+
+
+                    //assign the uvindexEl the value from the api call result    
+                    uvindexEl.innerHTML = "UV Index: " + uvIndex + "   ";
+                    
+                    
+
+                    //add uvIndex to the page
+                    cityDisplayEl.appendChild(uvindexEl);
+                    cityDisplayEl.appendChild(uvIndexIconEl);
+
+
+                })
+            }
+            else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect to Open Weather Map")
+        })
+    }
 
 
 
